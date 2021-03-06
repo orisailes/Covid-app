45c79be68b9f1cd11c2e6eeaa6a4928a7c3c3652
Ori Sailes <77921279+orisailes@users.noreply.github.com>
2021-03-06 20:56:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+//!important declaring&functions.
+const loader = document.querySelector(`.lds-facebook`);
+const canvas = document.querySelector(`.canvas`)
+
 async function covidFetch(url) {
+    mainBtn.classList.add(`hidden`)
+    loader.classList.remove(`hidden`)
+    canvas.style.opacity = "0.5"
     try {
         const response = await fetch(url)
         const data = await response.json()
@@ -30,6 +37,30 @@ async function countriesFetch(url) {
     } catch {
         handleErr(`Fetch Fail`)
     }
+    loader.classList.add(`hidden`)
+    canvas.style.opacity = "1"
+    userDisplayChoose.classList.remove(`hidden`)
+}
+
+function chartTypeByUser(type = `line`) {
+    userDisplayChoiseDiv.style.animation = "myDrop 1s forwards";
+    canvas.style.opacity = "0.5"
+    userDisplayChoose.classList.remove(`hidden`)
+    userDropdownOptions.forEach((e) => {
+        e.classList.remove(`hidden`)
+        e.addEventListener(`click`, changeType)
+    })
+
+}
+
+function changeType(e) {
+    console.log(e.currentTarget.textContent.toLowerCase().split(' '))
+    myChart.config.type = e.currentTarget.textContent.toLowerCase().split(' ')[0];
+    userDisplayChoose.classList.add(`hidden`)
+    mainBtn.classList.remove(`hidden`)
+    userChangeTypeBtn.classList.remove(`hidden`)
+    myChart.update();
+    canvas.style.opacity = "1"
 }
 
 function handleErr(err = `something went wrong`) {
@@ -38,6 +69,14 @@ function handleErr(err = `something went wrong`) {
     errorDiv.classList.remove(`hidden`)
 }
 
+const userDisplayChoiseDiv = document.querySelector(`.flex-options`)
+const userChangeTypeBtn = document.querySelector(`.change-type-btn`)
+userChangeTypeBtn.addEventListener(`click`, chartTypeByUser)
+const userDisplayChoose = document.querySelector(`.user-display-choose`)
+const userDropdownOptions = document.querySelectorAll(`.opt`)
+const userDropdown = document.querySelector(`.user-display-choose>h3`)
+userDropdown.addEventListener(`click`, chartTypeByUser)
+const mainBtn = document.querySelector(`.buttons-container`)
 const statusBtns = document.querySelector(`.buttons-status-container`)
 const errorDiv = document.querySelector(`.error`)
 let deathRateBtn = document.querySelector(`#death-rate`)
@@ -52,11 +91,13 @@ countriesFetch(countriesEndpoint)
 let dataByRegion = [];
 let newLabelName = [];
 let newData = [];
+let found;
+
 
 //! chart
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'line',
+    type: `line`,
     data: {
         labels: [],
         datasets: [{
@@ -84,14 +125,17 @@ var myChart = new Chart(ctx, {
 });
 //! end chart
 
-
+//*make labels by click on one of regions buttons
 function updateChartsByClick(e) {
+    myChart.config.type = userCosenType ;
+    canvas.style.opacity = "0.5"
     statusBtns.classList.remove(`hidden`)
+    loader.classList.remove(`hidden`)
+
     try {
         errorDiv.classList.add(`hidden`)
         deathRateBtn.classList.add(`hidden`)
         recoveryRateBtn.classList.add(`hidden`)
-        document.querySelector(`.lds-facebook`).classList.remove(`hidden`)
         //!array for all match data(names&covid status)
         dataByRegion = [];
         //!array for all new names(x)
@@ -100,7 +144,7 @@ function updateChartsByClick(e) {
         newData = [];
         //!the button text content plays the region finding role
         let region = e.currentTarget.textContent;
-        if(region===`World`){
+        if (region === `World`) {
             countriesMap.forEach(element => {
                 dataByRegion.push({
                     name: element.name,
@@ -144,13 +188,17 @@ function updateChartsByClick(e) {
     document.querySelector(`#myChart`).classList.remove(`hidden`)
     document.querySelector(`.buttons-status-container`).classList.remove(`hidden`)
     makeCountriesDivs()
-    document.querySelector(`.lds-facebook`).classList.add(`hidden`)
+    loader.classList.add(`hidden`)
+    canvas.style.opacity = "1"
 }
 
+//*small countries bottuns below
 function makeCountriesDivs() {
+    myChart.config.type = userCosenType ;
+    canvas.style.opacity = "0.5"
     statusBtns.classList.remove(`hidden`)
     errorDiv.classList.add(`hidden`)
-    document.querySelector(`.lds-facebook`).classList.remove(`hidden`)
+    loader.classList.remove(`hidden`)
     try {
         const textContainer = document.querySelector(`.text`);
         textContainer.innerHTML = ``
@@ -167,26 +215,31 @@ function makeCountriesDivs() {
     } catch {
         (handleErr(`Fail to display countries`))
     }
-    document.querySelector(`.lds-facebook`).classList.add(`hidden`)
+    loader.classList.add(`hidden`)
+    canvas.style.opacity = "1"
 }
 
+//*coloring
 function changeColor(color) {
     for (let i = 0; i < newData.length; i++) {
         myChart.data.datasets[0].backgroundColor[i] = color
     }
 }
 
+//*display different status (deaths/confirmed/recovered/critical) 
 function updateDataByClick(e) {
+    myChart.config.type = userCosenType ;
+    canvas.style.opacity = "0.5"
     statusBtns.classList.remove(`hidden`)
-    //!the first line of buttons
     errorDiv.classList.add(`hidden`)
-    document.querySelector(`.lds-facebook`).classList.remove(`hidden`)
+    loader.classList.remove(`hidden`)
     newData = []
     myChart.data.datasets[0].label = `${e.currentTarget.textContent} cases`
     const status = e.currentTarget.textContent.toLowerCase();
     dataByRegion.forEach(e => {
         e.covidData ? newData.push(e.covidData[status]) : ``;
     })
+    //! send to colorring function by pressing status button
     switch (status) {
         case `deaths`:
             changeColor("rgba(0, 0, 0, 0.8)")
@@ -195,7 +248,7 @@ function updateDataByClick(e) {
             changeColor("rgba(229, 80, 0, 0.8)")
             break;
         case `recovered`:
-            changeColor("rgba(0, 238, 255, 0.8)")
+            changeColor("rgba(116,214,0,0.8)")
             break;
         case `critical`:
             changeColor("rgba(225, 0, 0, 0.8)")
@@ -203,17 +256,20 @@ function updateDataByClick(e) {
     }
     myChart.data.datasets[0].data = newData;
     myChart.update();
-    document.querySelector(`.lds-facebook`).classList.add(`hidden`)
+    loader.classList.add(`hidden`)
+    canvas.style.opacity = "1"
 }
 
+
+//* display chart details after user click specific country
 function displayIndividualCountry(e) {
+    myChart.config.type = 'bar' ;
     statusBtns.classList.add(`hidden`)
-    //!after user click specific country
     errorDiv.classList.add(`hidden`)
-    document.querySelector(`.lds-facebook`).classList.remove(`hidden`)
+    loader.classList.remove(`hidden`)
     newData = [];
     try {
-        const found = covidMap.find(element => {
+        found = covidMap.find(element => {
             return element.countryName === e.currentTarget.textContent ? element.countryCovidData : ``;
         })
         deathRateBtn.classList.remove(`hidden`)
@@ -228,16 +284,43 @@ function displayIndividualCountry(e) {
         myChart.data.datasets[0].label = e.currentTarget.textContent
         myChart.data.datasets[0].data = newData
         myChart.data.labels = [`Deaths`, `Confirmed`, `Recovered`, `Critical`, `Death Rate`, `Recovery Rate`]
-        if (newData[0] < 1000) {
-            changeColor("rgba(0, 255, 42, 0.8)")
-        } else {
-            changeColor("rgba(225, 0, 0, 0.8)")
-        }
+        myChart.data.datasets[0].backgroundColor = [
+            "rgba(0, 0, 0, 0.8)",
+            "rgba(229, 80, 0, 0.8)",
+            "rgba(2,137,0,0.8)",
+            "rgba(225, 0, 0, 0.8)",
+            "rgba(225, 0, 0, 0.8)",
+            "rgba(2, 137, 0, 0.8)",
+        ]
+        covidToday.classList.remove(`hidden`)
         myChart.update();
     } catch {
         handleErr(`Cant get ${e.currentTarget.textContent} covid data.`)
     }
-    document.querySelector(`.lds-facebook`).classList.add(`hidden`)
+    loader.classList.add(`hidden`)
+}
+
+//*display covid today data
+function displayCovidToday() {
+    loader.classList.remove(`hidden`)
+    errorDiv.classList.add(`hidden`)
+    try {
+        newData = [];
+        newData.push(found.countryCovidDataToday.deaths)
+        newData.push(found.countryCovidDataToday.confirmed)
+        myChart.data.datasets[0].data = newData
+        myChart.data.labels = [`Deaths Today`, `Confirmed Today`]
+        if (newData[0] === 0 || newData[1] === 0) {
+            errorDiv.classList.remove(`hidden`)
+        }
+
+        myChart.config.type = "bar";
+    } catch {
+        handleErr(`fail to display today data`)
+    }
+    covidToday.classList.add(`hidden`)
+    loader.classList.add(`hidden`)
+    myChart.update();
 }
 
 
@@ -265,3 +348,8 @@ const recoverdBtn = document.querySelector(`#recovered`)
 recoverdBtn.addEventListener('click', updateDataByClick)
 const criticalBtn = document.querySelector(`#critical`)
 criticalBtn.addEventListener('click', updateDataByClick)
+const covidToday = document.querySelector(`.covid-today`)
+covidToday.addEventListener(`click`, displayCovidToday)
+
+//*chart type
+let userCosenType = myChart.config.type;
